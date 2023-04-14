5f4964427d529fa94983c180a190ef80f9cae19f
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-14 08:42:55 -0500

    problem 002
    Area of circle

---
+++ 01_Simple/Problem_002/p002.py
@@ -0,0 +1,9 @@
+'''
+Write A Python Program To Calculate The Area Of Circle Using Formula Πr2
+'''
+
+radius = float(input("Enter a radius : "))
+pi = 3.14
+
+area_circle = pi * radius**2
+print("Area of Circle is : ", round(area_circle))
