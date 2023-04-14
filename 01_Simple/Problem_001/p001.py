76c5dea258306ee6a2cdf3c76ffc7bfa3c49e26a
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-14 08:15:08 -0500

    Add files via upload

---
+++ 01_Simple/Problem_001/p001.py
@@ -0,0 +1,19 @@
+''''
+Write a program to show such type of layout of number and square
+Expect result : 
+
+    Number      Square 
+       1          1
+       2          4
+       3          9
+       4         16
+       5         25
+
+'''
+
+print("Number \t\t Square")
+print("1 \t\t "+str(1*1)) 
+print("2 \t\t "+str(2*2)) 
+print("3 \t\t "+str(3*3)) 
+print("4 \t\t "+str(4*4)) 
+print("5 \t\t "+str(5*5)) 
