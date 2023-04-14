895867409ce2aed6569118194814f96294710bf3
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-14 08:44:22 -0500

    Assignment 002
    Area of Rectangle

---
+++ 01_Simple/Problem_002/assignment_002.py
@@ -0,0 +1,10 @@
+"""
+Write A Python Program To Calculate The Area Of Rectangle using formula (Length * Width)
+"""
+
+length = float(input("Enter length : "))
+width = float(input("Enter width : "))
+
+area_rectangle = length * width
+
+print("Area of rectangle : ", round(area_rectangle,2))
