8046b29ad287942dae34d3d5f7a122a1fe5aa03a
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-14 08:27:48 -0500

    assignment 1

---
+++ 01_Simple/Problem_001/assignment001.py
@@ -0,0 +1,19 @@
+''''
+Write a program to show such type of layout of number, square and cube
+Expect result : 
+
+    Number      Square    Cube
+       1          1         1
+       2          4         8
+       3          9        27
+       4         16        64
+       5         25       125
+
+'''
+
+print("Number \t\t Square \tCube")
+print("1 \t\t "+str(1*1) + "\t\t" + str(pow(1,3))) 
+print("2 \t\t "+str(2*2) + "\t\t" + str(pow(2,3))) 
+print("3 \t\t "+str(3*3) + "\t\t" + str(pow(3,3)))  
+print("4 \t\t "+str(4*4) + "\t\t" + str(pow(4,3))) 
+print("5 \t\t "+str(5*5) + "\t\t" + str(pow(5,3))) 
