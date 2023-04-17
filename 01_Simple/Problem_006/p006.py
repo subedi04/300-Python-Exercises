2e3d8cf3e50215c6386060e5d796ed8c9931faf1
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-16 23:23:33 -0500

    problem 006

---
+++ 01_Simple/Problem_006/p006.py
@@ -0,0 +1,10 @@
+'''
+PROBLEM 6
+Write a Python Program to calculate union of two set
+A = {3,2,4,5,6,7,8} B = {4,12,5,1,6,8}
+'''
+A = {3,2,4,5,6,7,8}
+B = {4,12,5,1,6,8}
+
+c = A.union(B)
+print(c)
