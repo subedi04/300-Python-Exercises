4140cc89344ee2ae607e91c22f8e10236a7130ff
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-19 21:52:46 -0500

    Add files via upload

---
+++ 01_Simple/Problem_008/assignment_008.py
@@ -0,0 +1,11 @@
+'''
+Write A Python Program To Calculate Symmetric Difference Of Two Set
+A = {12,2,0,3,8} , B = {15,10,12,3,6}
+'''
+
+A = {12,2,0,3,8}
+B = {15,10,12,3,6}
+
+C = A.symmetric_difference(B)
+# C = A^B
+print(C)
