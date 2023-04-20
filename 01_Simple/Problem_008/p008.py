72d6b94915fbb93f5eec7435e530a3f141d82b02
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-19 21:47:56 -0500

    Problem 008

---
+++ 01_Simple/Problem_008/p008.py
@@ -0,0 +1,11 @@
+'''
+Write a Python Program to calculate difference of two set
+A = {1,12,2,6,7,8} B = {15,0,1,3,6}
+'''
+
+a = {23,5,1,12,4,6,7}
+b = {6,11,12,4,5,2,15,21,22,33}
+c = b.difference(a)
+print(c) 
+# a -b ? item of a that are not present in the b set
+# b -a ? item of b that are not present in the a set
