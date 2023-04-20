ef6aaeff7fd175f6ceff141d90d7adfd6310a604
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-19 21:35:16 -0500

    Problem 007

---
+++ 01_Simple/Problem_007/p007.py
@@ -0,0 +1,13 @@
+'''
+PROBLEM 7
+Write a Python Program to calculate intersection of two set
+A = {12,4,5,7,8,9} B = {1,12,33,8,0,9}
+'''
+
+print("Intersection of Two SET")
+
+a = {12,4,5,7,8,9}
+b = {1,12,33,8,0,9}
+c = a.intersection(b)
+
+print(c)
