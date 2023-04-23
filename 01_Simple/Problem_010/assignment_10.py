d762a4e31f3e109c4cc7b2fb65d2f96f479c8992
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-22 21:22:25 -0500

    Add files via upload

---
+++ 01_Simple/Problem_010/assignment_10.py
@@ -0,0 +1,37 @@
+'''
+Write A Python Program to get 6 subject marks from the user and calculate total, 
+average and percentage of that marks. And display to user.
+'''
+
+def Enter_Marks():
+    print("You must enter 6 marks")
+    marks = []
+    for i in range(0,6):
+        num = float(input("Enter your mark number : "))
+        marks.append(num)
+    return marks
+
+def Sum_Total(marks):
+    total = 0
+    for i in range(0,len(marks)):
+        total += marks[i]
+    return total
+
+def Average(marks):
+    return Sum_Total(marks)/len(marks)
+
+def Percent_Marks(marks):
+    print("\nPercentage of the numbers")
+    total = Sum_Total(marks)
+    for i in range(0,len(marks)):
+        print(marks[i]," is the ",round((marks[i]*100)/total,2),"%\tof total.")
+
+
+
+
+if __name__=="__main__":
+    marks = Enter_Marks()
+    print("\n")
+    print("The sum is : ",Sum_Total(marks))
+    print("The average is : ",Average(marks))
+    Percent_Marks(marks)
