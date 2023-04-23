638b2c048b060177fdad1a9400272ede3186b908
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-23 14:27:27 -0500

    Assignment_011

--- 01_Simple/Problem_011/assignment_011.py
+++ 01_Simple/Problem_011/assignment_011.py
@@ -14,7 +14,9 @@ def Max_3_numbers(n1,n2,n3):
 
 if __name__=="__main__":
     print("Found the maximun of three numbers")
+    
     n1 = float(input("Enter a number : "))
     n2 = float(input("Enter a number : "))
     n3 = float(input("Enter a number : "))
+    
     print("The max number is : ", Max_3_numbers(n1,n2,n3))
