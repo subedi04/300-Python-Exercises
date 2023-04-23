dd4fdc44c3e6186e3e4f6888bc7da162b7496e03
alexandercooper97 <130676645+alexandercooper97@users.noreply.github.com>
2023-04-23 14:26:57 -0500

    Assignment

--- 01_Simple/Problem_011/assignment_011.py
+++ 01_Simple/Problem_011/assignment_011.py
@@ -4,18 +4,13 @@ Write A Python Program To Get 3 Number From The User And Display Maximum Number
 
 def Max_3_numbers(n1,n2,n3):
     max = n1
-    if max >n2 and max > n3 : 
-        return max
-    elif n2 > max and n2 > n3:
+    if n2>max:
         max = n2
-        return max
-    elif n3 > max and n3 > n2 :
+    if n3 > max:
         max = n3
-        return max
-    else: 
+    if max==n1 and max ==n2: 
         print("Equals Numbers")
-
-
+    return max
 
 if __name__=="__main__":
     print("Found the maximun of three numbers")
