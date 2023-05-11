8a4a8740829f3c17f6af6b067fd40fee976e8850
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-10 23:52:10 -0500

    Problem 024

--- 01_Simple/Problem_024/p024.py
+++ 01_Simple/Problem_024/p024.py
@@ -5,47 +5,19 @@ Write A Python Program To Get Two Number And Operator From The User To Perform A
 # one operator 
 # perform arithmetic operations 
 
-def Arithmetic_Ops(a,b,op):
-    print("\n")
-    if(op == '+'):
-        print("Addition of two numbers is = "+str(a+b))
-    elif(op == '-'):
-        print("Subtraction of two numners is = "+str(a-b))
-    elif(op == '/'):
-        print("Division of two numbers is = "+str(a/b))
-    elif(op == '*'):
-        print("Multiplication of two numbers is = "+str(a*b))
-    elif(op == '%'):
-        print("Mod of two numbers is = "+str(a%b))
-    else:
-        print("Invalid Operator, please one of them as +, -, /, * and %")
+a =  int(input("Enter 1st numebr")) # 4
+b = int(input("Enter 2nd numebr")) # 7
+op = input("Insert a operator to perform operation") # /
 
-def Get_Data():
-    a = int(input(">>> Enter 1st number : ")) # 4
-    b = int(input(">>> Enter 2nd number : ")) # 7
-    op = input(">>> Insert a operator to perform operation : ") # /
-    return a,b,op
-
-def Info():
-    print("\t*****************************************************************")
-    print("\t**                                                             **")
-    print("\t**                       BASIC OPERATIONS                      **")
-    print("\t**                                                             **")
-    print("\t*****************************************************************")
-
-    print("You must enter 2 numbers and chose your operator\n")
-
-    print("Operators \t Operation")
-    print(" + \t\t addition")
-    print(" - \t\t sustraction")
-    print(" * \t\t multiplication")
-    print(" / \t\t division")
-    print(" % \t\t module")
-    print("\n")
-
-
-
-if __name__=="__main__":
-    Info()
-    a,b,op = Get_Data()
-    Arithmetic_Ops(a,b,op)
+if(op == '+'):
+    print("Addition of two numebr is = "+str(a+b))
+elif(op == '-'):
+    print("Subtraction of two numebr is = "+str(a-b))
+elif(op == '/'):
+    print("Division of two numebr is = "+str(a/b))
+elif(op == '*'):
+    print("Multiplication of two numebr is = "+str(a*b))
+elif(op == '%'):
+    print("Mod of two numebr is = "+str(a%b))
+else:
+    print("Invalid Operator, please one of them as +, -, /, * and %")
