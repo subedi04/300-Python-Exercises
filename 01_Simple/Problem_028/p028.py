734dfbe90c9a2d8f082d3ebd718cfb1d06d39b09
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-11 09:45:00 -0500

    Problerm 028

---
+++ 01_Simple/Problem_028/p028.py
@@ -0,0 +1,15 @@
+# to get a number 
+# square of that number
+
+def square_number(n):
+    square = n*n
+    print("Square of "+str(n)+" is = "+str(square))
+
+if __name__ =="__main__":
+    n = int(input("Enter a number : "))
+    square_number(n)
+
+
+# 5
+#Square of 5 is 25
+#Square of 5 is 36
