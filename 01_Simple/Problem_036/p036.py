0c55be0295906bcca612ac00aaa49c433437544f
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-13 00:23:25 -0500

    Problem 036

---
+++ 01_Simple/Problem_036/p036.py
@@ -0,0 +1,9 @@
+'''
+Write A Python Program To Get A Sentence Form The User, And Make It Capitalize
+'''
+# to get sentence from the user
+# convert into cap
+
+sentence = input("Enter a sentence ")
+cap_sent = sentence.capitalize() # how are you, How are you
+print(cap_sent+".")
