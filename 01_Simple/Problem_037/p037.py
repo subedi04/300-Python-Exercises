fc147447be71cef8509e0e6a30bd47a20bd98409
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-13 00:28:09 -0500

    Problem 037

---
+++ 01_Simple/Problem_037/p037.py
@@ -0,0 +1,6 @@
+# to get a number from the user
+# find cub
+
+n = int(input("Enter a number to get its cube : ")) # '5' -> 5
+print("You entered "+str(n)+" number")
+print("Cube of "+str(n)+" number is = "+str(n*n*n))
