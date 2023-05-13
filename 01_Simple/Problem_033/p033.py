8175e6377837846b140a627aac6d169102ac2ee2
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-12 23:50:09 -0500

    Problem 033

---
+++ 01_Simple/Problem_033/p033.py
@@ -0,0 +1,11 @@
+'''
+Write A Python Program To Store Name of the color, In The List, 
+And Count That Color And Display Result to User
+'''
+# to store color in list
+# count the color
+# display result to user
+
+lst = ['red','gray','blue','pink','green','yellow','black','white']
+print("Color in the list = "+str(lst))
+print("Total color in the list = "+str(len(lst)))
