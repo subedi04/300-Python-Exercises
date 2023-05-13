9e034c7ec6d005b67723ae77b7191e5cca0da6fb
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-13 00:25:44 -0500

    Problem 036

---
+++ 01_Simple/Problem_036/assignment_036.py
@@ -0,0 +1,10 @@
+'''
+Write A Python Program To Get A Sentence Form The User, And Convert all the characters into UPPERCASE. 
+For Example:
+User Sentence : How are you
+Converted: HOW ARE YOU
+'''
+
+sentence = input("Enter a sentence : ")
+cap_sent = sentence.upper() # how are you, How are you
+print(cap_sent+".")
