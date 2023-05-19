374e0d8f07bae7adb6cc6fc32820d8d24fca4907
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-05-18 21:18:22 -0500

    Problem 050

---
+++ 01_Simple/Problem_050/p050.py
@@ -0,0 +1,8 @@
+# to get base and height from the user
+# find area of the triangle 
+
+b = float(input("Enter Base   : "))
+h = float(input("Enter Height : "))
+
+area_traingle = (b * h)/2
+print("Area of the triangle is = "+str(area_traingle))
