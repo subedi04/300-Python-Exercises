a7fecaf8f82abfd59fd1a93c77a4f937c0af86f0
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-06-04 21:46:18 -0500

    Problem 058

---
+++ 01_Simple/Problem_058/p58.py
@@ -0,0 +1,14 @@
+'''
+Write A Python Program To get first and last name 
+from the user and display full name. 
+'''
+# to get first name
+# to get last name
+# display full name
+
+f_name = input("Enter your first name")
+l_name = input("Enter your last name")
+
+print("Your First name is "+f_name)
+print("Your Last name is "+l_name)
+print("Your Full name is "+f_name +" "+ l_name)
