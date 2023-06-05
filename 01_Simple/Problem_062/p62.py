eaae673f086443755fe67eec7c0fa029176e64d3
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-06-05 06:42:15 -0500

    Problem 062

---
+++ 01_Simple/Problem_062/p62.py
@@ -0,0 +1,24 @@
+'''
+Write A Python Program To Get Different Information Of Students 
+From User And Display All The Student Information In This Order.
+Name			---------
+Father name 		---------
+CNIC			---------
+Age 			---------
+Contact 		---------
+'''
+# to get std info
+# display to user
+
+name = input("Enter your full name : ")
+f_name = input("Enter your father  name : ")
+cnic = input("Enter your CNIC : ")
+age = input("Enter your age : ")
+contact = input("Enter your contact : ")
+
+print("This is a student informaiton that we get from user")
+print("Name \t\t "+name)
+print("Father Name \t\t "+f_name)
+print("CNIC \t\t "+cnic)
+print("Age \t\t "+age)
+print("Contact \t\t "+contact)
