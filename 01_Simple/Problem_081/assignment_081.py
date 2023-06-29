90fb4e639b2ba852757a56f6b271b8b30a92b2c6
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-06-29 18:44:53 -0500

    Problem 081

---
+++ 01_Simple/Problem_081/assignment_081.py
@@ -0,0 +1,7 @@
+'''
+Write A Python Program To Find Sum Of 
+2nd Number And The 2nd Last Number From 
+A List
+'''
+num_list = [2,3,4,5,6,7,12,15,9,43,24,1325,541]
+print(num_list[1] + num_list[-2])
