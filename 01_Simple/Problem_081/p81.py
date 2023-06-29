e30c27495d29319ba43119eb20b8616c99af1900
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-06-29 18:41:42 -0500

    Problem 081

---
+++ 01_Simple/Problem_081/p81.py
@@ -0,0 +1,10 @@
+'''
+Write A Python Program To Find Sum Of 1st Number 
+And The Last Number From A List
+'''
+# to create list
+# insert number
+# find sum of first and last element of list
+
+num_list = [2,3,4,5,6,7,12,15,9,43,24,1325,541]
+print(num_list[0] + num_list[-1])
