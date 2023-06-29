71b11456a7206351e278b6300a9176d2fdeb1b15
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-06-29 06:49:16 -0500

    Problem 079

---
+++ 01_Simple/Problem_079/p79.py
@@ -0,0 +1,10 @@
+'''
+Write A Python Program To Print First 
+And Last Characters From A String
+'''
+# to get sentence
+# dispaly first and last char of string
+
+s = input("Enter a sentence")
+print("Sentence first char is "+s[0])
+print("Sentence last char is "+s[-1])
