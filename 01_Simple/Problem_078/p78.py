d36418b5e1b95458bd4342953ea495330bcb900d
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-06-28 20:45:47 -0500

    Problem 078

---
+++ 01_Simple/Problem_078/p78.py
@@ -0,0 +1,14 @@
+'''
+Write A Python Program To Get A Number 
+To Return Its Next And Previous Number
+'''
+# to get a num 
+# find pre and next num
+
+num = int(input("Enter a number : "))
+print(num)
+#print(num+1)
+print(num-1)
+num += 1
+print(num) 
+ 
