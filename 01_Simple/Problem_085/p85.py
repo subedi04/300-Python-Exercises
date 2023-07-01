0e7dd9748bc55b33edb172ceffe20248fb3a5485
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-01 08:10:13 -0500

    Problem 085

---
+++ 01_Simple/Problem_085/p85.py
@@ -0,0 +1,9 @@
+# create list
+# display name that end at n
+
+if __name__=="__main__":
+    ls = ['ali','kami','kamran','yasin','faisal']
+    for i in ls:
+        if(i.endswith('n')):
+            print(i)
+    
