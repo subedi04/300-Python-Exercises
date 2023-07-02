7d954fd441ce730826ba4c1be57cb7630149a470
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-02 15:28:08 -0500

    Problem 092

---
+++ 01_Simple/Problem_092/p92.py
@@ -0,0 +1,11 @@
+'''
+Write A Python Program To Find Ceiling Of A Number
+'''
+# to get number
+# find ceiling of number
+
+import math
+
+if __name__=="__main__":
+    num = float(input("Enter a number : "))
+    print(math.ceil(num))
