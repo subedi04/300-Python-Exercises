242f23c1f87529f51297e8cb79828e1172ab5852
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-04 05:53:09 -0500

    Problem 098

---
+++ 01_Simple/Problem_098/p98.py
@@ -0,0 +1,10 @@
+lst = [4,5,1,10,34]
+l = len(lst)
+print("Length of list ="+str(l))
+print("Items of lsit ")
+for i in lst:
+	print(i)
+half = 1/2
+middle_el = int(half)
+#print(middle_el) #3
+print("Middle Element = "+str(lst[middle_el]))
