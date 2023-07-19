fac45f8d5bebd10ba3296c8c18d6cd8dde1292bf
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-19 07:21:15 -0500

    Problem 121

---
+++ 02_Intermediate/Problem_121/p121.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to create a nested list and access nested list item.
+'''
+
+n_list = [1,2,[3,4,5],[True,False],["Jafri","Faisal"]]
+print(n_list[3][1])
+# for i in n_list:
+#     print(i)
+# print(n_list)
