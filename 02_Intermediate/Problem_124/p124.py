92ea45705b1cd4f5525737ea28b3bfe575a1bd72
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-20 23:57:17 -0500

    Problem 124

---
+++ 02_Intermediate/Problem_124/p124.py
@@ -0,0 +1,7 @@
+'''
+Write a Python Program to Create a Nested Dictionary and Access Value.
+'''
+
+dic = {1:{"name":"Faisal Zamir"}, 2:"Red",3:{"age":20}}
+# print(dic[1]["name"])
+print(dic[3]["age"])
