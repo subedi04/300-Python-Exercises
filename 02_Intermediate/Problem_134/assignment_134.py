9ee4da86081e8a99de69edaaa8156c1a8d3f06b6
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-24 09:18:54 -0500

    Problem 134

---
+++ 02_Intermediate/Problem_134/assignment_134.py
@@ -0,0 +1,14 @@
+'''
+Write a Python Numpy Program to 
+Create an array of the integers 
+from 0 to 50
+'''
+
+import numpy as np
+
+if __name__ == "__main__":
+    # Create an array of integers from 0 to 50
+    array_of_integers = np.arange(51)
+
+    print("Array of integers from 0 to 50:")
+    print(array_of_integers)
