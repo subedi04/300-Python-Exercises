b1df8168bf899c3f3eddbc7c2388dc0727984846
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-27 06:43:34 -0500

    Problem 139

---
+++ 02_Intermediate/Problem_139/assignment_139.py
@@ -0,0 +1,28 @@
+'''
+Write a NumPy program to create a new array of 
+given shape (4,3) and type, filled with ones. 
+And make in such way, user give shape value 
+on run time.
+'''
+
+import numpy as np
+
+def create_ones_array():
+    while True:
+        try:
+            rows = int(input("Enter the number of rows: "))
+            cols = int(input("Enter the number of columns: "))
+            break
+        except ValueError:
+            print("Invalid input! Please enter integer values for rows and columns.")
+
+    ones_array = np.ones((rows, cols), dtype=np.int)
+    return ones_array
+
+def main():
+    ones_array = create_ones_array()
+    print("Array of ones:")
+    print(ones_array)
+
+if __name__ == "__main__":
+    main()
