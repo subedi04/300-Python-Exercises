8f480aa0d240279a1f1962c3bff2ffbbf0b870f2
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-28 19:30:18 -0500

    Problem 142

---
+++ 02_Intermediate/Problem_142/p142.py
@@ -0,0 +1,9 @@
+'''
+Write a Python Program to Reverse a Array.
+'''
+import numpy as np
+if __name__=="__main__":
+    ar = np.array([1,2,3,4,5,6])
+    print(ar)
+    r_ar = np.flip(ar)
+    print(r_ar)
