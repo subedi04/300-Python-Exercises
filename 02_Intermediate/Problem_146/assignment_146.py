de253b45b8c70da3c6442175ee3d715047e282e3
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-07-30 21:59:25 -0500

    Problem 146

---
+++ 02_Intermediate/Problem_146/assignment_146.py
@@ -0,0 +1,10 @@
+'''
+Write a Python Numpy program to create a 4x4 zero matrix.
+'''
+
+import numpy as np
+
+if __name__=="__main__":
+    # Create a 4x4 zero matrix
+    zero_matrix = np.zeros((4, 4))
+    zero_matrix
