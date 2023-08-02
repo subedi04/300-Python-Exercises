373ad33c071563420788b3ba8ec2a2dab51468a5
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-08-01 23:58:05 -0500

    Problem 153

---
+++ 02_Intermediate/Problem_153/p153.py
@@ -0,0 +1,30 @@
+'''
+Write a NumPy program to create a three dimension array 
+with shape (3,4,4) and set to a variable
+'''
+import numpy as np
+
+ar = np.array([
+    [
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4]
+    ],
+    [
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4]
+
+    ],
+    [
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4],
+        [1,2,3,4]
+
+    ]
+])
+
+print(ar)
