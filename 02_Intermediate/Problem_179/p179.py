6588ba1c5bd0c0c13d4a90137f59bb5ca8a65a76
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-08-12 23:22:54 -0500

    Problem 179

---
+++ 02_Intermediate/Problem_179/p179.py
@@ -0,0 +1,11 @@
+'''
+Create a Desktop Python program to create a window and set its title
+'''
+
+from tkinter import * 
+
+if __name__=="__main__":
+    root = Tk()
+    root.title("This is the title of the window")
+    root1 = Tk()
+    root.mainloop()
