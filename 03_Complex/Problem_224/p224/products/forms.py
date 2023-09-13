ed88158cdce122dc9620702bbef79b9caeb5bfb6
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-09-13 06:49:54 -0500

    Problem 224

---
+++ 03_Complex/Problem_224/p224/products/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class Productsform(forms.Form):
+    p1 = forms.IntegerField()
+    p2 = forms.IntegerField()
+    p3 = forms.IntegerField()
+    p4 = forms.IntegerField()
+    p5 = forms.IntegerField()
+    p6 = forms.IntegerField()
+    p7 = forms.IntegerField()
+    p8 = forms.IntegerField()
+    p9 = forms.IntegerField()
+    p10 = forms.IntegerField()
