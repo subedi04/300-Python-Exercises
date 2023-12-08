a874cedfd07be55c5e2fa17a36b21f59a7d04eb2
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-08 09:38:08 -0500

    Problem 279

---
+++ 03_Complex/Problem_279/p279/addlistitem/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class ListItem(forms.Form):
+    l1 = forms.IntegerField()
+    l2 = forms.IntegerField()
+    l3 = forms.IntegerField()
+    l4 = forms.IntegerField()
+    l5 = forms.IntegerField()
