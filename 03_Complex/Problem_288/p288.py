9787f222aafec9731bf1ee7f12bd6e1a86ad906a
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-21 23:43:55 -0500

    Problem 288

---
+++ 03_Complex/Problem_288/p288.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+messagebox.showerror("Error title","This is the error in your system")
+messagebox.showinfo("information title","We are provding something to you")
+messagebox.showwarning("Warning title","Dont use again this one")
+
+root.mainloop()
