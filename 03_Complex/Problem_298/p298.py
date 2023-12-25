4480f2231ad421e009ae1cd290be51a0ce686f44
Alexander Miguel Cooper <130676645+alexandercooper97@users.noreply.github.com>
2023-12-25 13:42:10 -0500

    Problem 298

---
+++ 03_Complex/Problem_298/p298.py
@@ -0,0 +1,29 @@
+from tkinter import *
+root = Tk()
+root.geometry("400x300")
+
+def get_colors():
+    color1 = entry1.get()
+    color2 = entry2.get()
+    color3 = entry3.get()
+    color4 = entry4.get()
+    color5 = entry5.get()
+    lst = [color1, color2, color3, color4,color5]
+    for i in lst:
+        if i != lst[0] and i != lst[-1]:
+            item = i
+            Label(root, text= item).pack()
+
+entry1 = Entry(root, width=40)
+entry1.pack(pady=4)
+entry2 = Entry(root, width=40)
+entry2.pack(pady=4)
+entry3 = Entry(root, width=40)
+entry3.pack(pady=4)
+entry4 = Entry(root, width=40)
+entry4.pack(pady=4)
+entry5 = Entry(root, width=40)
+entry5.pack(pady=4)
+btn = Button(root, text="Enter", command=get_colors)
+btn.pack()
+root.mainloop()
